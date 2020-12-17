dc028ceb4270afd6ad5aa5e0b0d49dc0c49d8db0
hongyuanhua <hhy981123@gmail.com>
2020-12-17 01:04:02 -0500

    input box animation added

--- pub/inputValidator.js
+++ pub/inputValidator.js
@@ -23,43 +23,43 @@ function inputListener(id, type) {
 
 function pwdValidate(box) {
   if (box[0].value === "") {
-    box.next()[0].innerHTML = "<br>Password cannot be empty!";
+    box.next()[0].innerHTML = "Password cannot be empty!";
     box.next().attr("class", "message_warning");
   } else if (
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(
       box[0].value
     )
   ) {
-    box.next()[0].innerHTML = "<br>Password is valid!";
+    box.next()[0].innerHTML = "Password is valid!";
     box.next().attr("class", "message");
   } else {
     box.next()[0].innerHTML =
-      "<br>Password should between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character!";
+      "Password should between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character!";
     box.next().attr("class", "message_warning");
   }
 }
 
 function repwdValidate(box) {
   if (box[0].value === "") {
-    box.next()[0].innerHTML = "<br>Password cannot be empty!";
+    box.next()[0].innerHTML = "Password cannot be empty!";
     box.next().attr("class", "message_warning");
   } else if ($("#rpwd")[0].value === box[0].value) {
-    box.next()[0].innerHTML = "<br>Password is matched!";
+    box.next()[0].innerHTML = "Password is matched!";
     box.next().attr("class", "message");
   } else {
-    box.next()[0].innerHTML = "<br>Password does not match!";
+    box.next()[0].innerHTML = "Password does not match!";
     box.next().attr("class", "message_warning");
   }
 }
 function textValidate(box) {
   if (box[0].value === "") {
-    box.next()[0].innerHTML = "<br>Input cannot be empty!";
+    box.next()[0].innerHTML = "Input cannot be empty!";
     box.next().attr("class", "message_warning");
   } else if (/\d/.test(box[0].value)) {
-    box.next()[0].innerHTML = "<br>Input should not contain numbers!";
+    box.next()[0].innerHTML = "Input should not contain numbers!";
     box.next().attr("class", "message_warning");
   } else {
-    box.next()[0].innerHTML = "<br>Input is valid!";
+    box.next()[0].innerHTML = "Input is valid!";
     box.next().attr("class", "message");
   }
 }
@@ -67,17 +67,17 @@ function emailValidate(box) {
   //   console.log(box[0].value);
   //   console.log(box.next());
   if (box[0].value === "") {
-    box.next()[0].innerHTML = "<br>Input cannot be empty!";
+    box.next()[0].innerHTML = "Input cannot be empty!";
     box.next().attr("class", "message_warning");
   } else if (
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
       box[0].value
     )
   ) {
-    box.next()[0].innerHTML = "<br>The email address is valid!";
+    box.next()[0].innerHTML = "The email address is valid!";
     box.next().attr("class", "message");
   } else {
-    box.next()[0].innerHTML = "<br>This is not a valid email address!";
+    box.next()[0].innerHTML = "This is not a valid email address!";
     box.next().attr("class", "message_warning");
   }
 }
